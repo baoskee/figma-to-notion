4083fdf917a67b88531c8d0733574c3fde330564
Bao <baomai@berkeley.edu>
2022-02-12 09:59:58 -0800

    Completed iteration 1

--- README.md
+++ README.md
@@ -11,10 +11,14 @@ programmatically. Figjam file hierarchy:
 
 # Design
 
-Attributes to Notion: title (text), project (tag), Figma URL (URL),
-last modified (in Figma), key (text). The last attribute is to sync your Figma files
-and update their other attributes without misplacing it; the key is also
-used to generate the Figma URL.
+Attributes to Notion:
+
+- File name (text)
+- Project (tag)
+- Figma URL (URL)
+- Last modified (in Figma)
+
+The Figma URL contains a `key` attribute in which you can identify the files. The URL is appended with the Figma File ID.
 
 The Notion table can add additional decorator columns.
 Post-compilation can change text and project and will not replace Notion
@@ -27,4 +31,4 @@ In your .env file, make sure that the variables are populated
 - `FIGMA_SECRET` - Access token to your Figma workspaces
 - `FIGMA_TEAM` - The ID for Figma team you want to compile files to
 - `NOTION_SECRET` - The Notion secret to workspace you want to compile to
-- `NOTION_TABLE` - The ID of database target to inject your Figma files
+- `NOTION_TARGET` - The ID of database target to inject your Figma files
--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
+import { Client } from '@notionhq/client'
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
 import axios from 'axios'
 import 'dotenv/config'
 
 const FIGMA_SECRET = process.env.FIGMA_SECRET as string
 const FIGMA_TEAM = process.env.FIGMA_TEAM
 const NOTION_SECRET = process.env.NOTION_SECRET
-const NOTION_TARGET = process.env.NOTION_TARGET
+const NOTION_TARGET = process.env.NOTION_TARGET as string
 
 const FIGMA_API_CONF = {
   headers: { ['X-FIGMA-TOKEN']: FIGMA_SECRET },
 }
+
+const notion = new Client({ auth: NOTION_SECRET })
 
 type Team = {
   name: string
@@ -25,7 +29,49 @@ type File = {
   last_modified: string
 }
 
+const figmaFileToPage = (projectName: string, file: File) => ({
+  Name: {
+    type: 'title',
+    title: [{ type: 'text', text: { content: file.name } }],
+  },
+  Project: {
+    select: {
+      name: projectName,
+    },
+  },
+  'Figma URL': {
+    url: `https://www.figma.com/file/${file.key}`,
+  },
+  'Last modified': {
+    date: { start: file.last_modified },
+  },
+  // TODO: Include thumbnail URL
+})
+
+const lastParam = (url: string): string => {
+  const tokens = url.split('/')
+  return tokens[tokens.length - 1]
+}
+
+const fetchTablePages = async (
+  tableId: string,
+  nextCursor?: string
+): Promise<QueryDatabaseResponse['results']> => {
+  const res = await notion.databases.query({
+    database_id: tableId,
+    start_cursor: nextCursor,
+  })
+  if (res.has_more)
+    return {
+      ...res.results,
+      ...(await fetchTablePages(tableId, res.next_cursor!)),
+    }
+
+  return res.results
+}
+
 const main = async () => {
+  // Get all team's projects
   const res = await axios.get(
     `https://api.figma.com/v1/teams/${FIGMA_TEAM}/projects`,
     FIGMA_API_CONF
@@ -44,6 +90,60 @@ const main = async () => {
   const allFiles: { name: string; files: File[] }[] = projectFilesRes.map(
     (o) => o.data
   )
+  const pages = await fetchTablePages(NOTION_TARGET)
+
+  // Map of Figma key to Notion page, but sourced from existing
+  // pages in Notion table
+  const notionPageTable: Map<string, string> = pages.reduce((m, page) => {
+    const figmaFileKey = lastParam(page['properties']['Figma URL'].url)
+    m.set(figmaFileKey, page.id)
+    return m
+  }, new Map())
+
+  // Remove pages that were deleted from Figma, Notion key set - Figma key set
+  const figmaKeysFromNotion = [...notionPageTable.keys()]
+  const figmaKeys = new Set(
+    allFiles
+      .map((proj) => proj.files)
+      .flat()
+      .map((f) => f.key)
+  )
+
+  // MARK: Delete Notion pages that are no longer in Figma
+  for (let key of figmaKeysFromNotion.filter((k) => figmaKeys.has(k))) {
+    await notion.pages.update({
+      page_id: notionPageTable.get(key)!,
+      archived: true,
+    })
+  }
+
+  // MARK: Update or insert files from Figma to Notion
+  for (let project of allFiles) {
+    for (let file of project.files) {
+      // Update existing Notion page if found
+      if (notionPageTable.has(file.key)) {
+        await notion.pages.update({
+          page_id: notionPageTable.get(file.key)!,
+          cover: {
+            external: { url: file.thumbnail_url },
+          },
+          // @ts-ignore
+          properties: figmaFileToPage(project.name, file),
+        })
+        continue
+      }
+
+      // Create if not found
+      await notion.pages.create({
+        parent: { database_id: NOTION_TARGET },
+        cover: {
+          external: { url: file.thumbnail_url },
+        },
+        // @ts-ignore
+        properties: figmaFileToPage(project.name, file),
+      })
+    }
+  }
 }
 
 main()
